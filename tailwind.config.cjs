5f609118da7ec7e60e67596f4820ecd246f0a2ae
josh <jloomb@gmail.com>
2022-07-18 20:44:18 +1200

    Install tailwindcss and daisyui

---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+                           
+          "primary": "#0D0D0D",
+                                          
+          "secondary": "#1A1919",
+                                          
+          "accent": "#262626",
+                                          
+          "neutral": "#000000",
+                                          
+          "base-100": "#FFFFFF",
+                                          
+          "info": "#0072F5",
+                                          
+          "success": "#21CA51",
+                                          
+          "warning": "#FF6052",
+                                          
+          "error": "#DE1B8D",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
+}
